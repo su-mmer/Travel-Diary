a929b585cee24eac5a7bae2d91c800c5175169b1
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-04-09 22:47:49 +0900

    Update DiaryController.java
    controller 기능 추가

--- back/src/main/java/com/traveldiary/controller/DiaryController.java
+++ back/src/main/java/com/traveldiary/controller/DiaryController.java
@@ -1,10 +1,14 @@
 package com.traveldiary.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.traveldiary.data.dto.DiaryDto;
+import com.traveldiary.data.requestdata.RequestDiaryData;
 import com.traveldiary.exception.DiaryJpaException;
 import com.traveldiary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
@@ -31,10 +35,36 @@ public class DiaryController {
   /************************
    * POST *
    ************************/
+  @PostMapping("/diary")
+  public ResponseEntity<DiaryDto> saveDiary(RequestDiaryData data) {
+    return ResponseEntity.ok(diaryService.saveDiary(data));
+  }
+
   /************************
    * PUT *
    ************************/
+  @PutMapping("/diary/{id}")
+  public ResponseEntity<DiaryDto> updateDiary(@PathVariable("id") Long id, RequestDiaryData data) {
+    try {
+      DiaryDto diaryDto = diaryService.updateDiary(id, data);
+      return ResponseEntity.ok(diaryDto);
+    } catch (DiaryJpaException e) {
+      e.printStackTrace();
+      return ResponseEntity.badRequest().build();
+    }
+  }
+
   /************************
    * DELETE *
    ************************/
+  @DeleteMapping("/diary/{id}")
+  public ResponseEntity<DiaryDto> deleteDiary(@PathVariable Long id) {
+    try {
+      diaryService.deleteDiary(id);
+      return ResponseEntity.ok(null);
+    } catch (DiaryJpaException e) {
+      e.printStackTrace();
+      return ResponseEntity.badRequest().build();
+    }
+  }
 }
