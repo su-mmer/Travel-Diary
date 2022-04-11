91e73550cfb69dc547ef009133817fb799f7ec31
Apisapple <38449976+Apisapple@users.noreply.github.com>
2022-04-12 02:19:32 +0900

    backend_DB 연결 및 entity 매핑
    backend_DB 연결 및 entity 매핑

--- back/src/main/java/com/traveldiary/domain/Diary.java
+++ back/src/main/java/com/traveldiary/domain/Diary.java
@@ -1,17 +1,21 @@
 package com.traveldiary.domain;
 
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @Entity(name = "diary")
+@NoArgsConstructor
 @AllArgsConstructor
 public class Diary {
 
@@ -24,6 +28,9 @@ public class Diary {
 
   @Column(name = "content")
   private String content;
+
+  @OneToMany(mappedBy = "diary")
+  private Set<ImageFile> imageFiles;
 
   public void updateDiaryData(String title, String content) {
     this.title = title;
--- back/src/main/java/com/traveldiary/domain/ImageFile.java
+++ back/src/main/java/com/traveldiary/domain/ImageFile.java
@@ -1,5 +1,44 @@
 package com.traveldiary.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImageFile {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  private Diary diary;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "path")
+  private String path;
+
+  @Override
+  public String toString() {
+    return "ImageFile{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        '}';
+  }
 }
+
+
